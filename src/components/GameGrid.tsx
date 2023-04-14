cada6a451414c3fbb90ee16d9fbf19525a48459a
rohit-kumar-globant <108795419+rohit-kumar-globant@users.noreply.github.com>
2023-04-14 19:37:41 +0530

    filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,16 @@
 import { SimpleGrid, Text } from "@chakra-ui/react"
 import useGames from "../hooks/useGame"
+import { Genre } from "../hooks/useGenre";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
 
-const GameGrid = () => {
-    const { data, error, isLoading } = useGames();
+interface Props {
+    selectedGenre: Genre | null
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+    const { data, error, isLoading } = useGames(selectedGenre);
     const skeletons = [1, 2, 3, 4, 5, 6];
 
 
@@ -14,13 +19,13 @@ const GameGrid = () => {
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='10px' spacing={3}>
                 {isLoading && skeletons.map(skeleton =>
-                    <GameCardContainer>
-                        <GameCardSkeleton key={skeleton} />
+                    <GameCardContainer key={skeleton}>
+                        <GameCardSkeleton />
                     </GameCardContainer>
                 )}
                 {data.map(game => (
-                    <GameCardContainer>
-                        <GameCard key={game.id} game={game} />
+                    <GameCardContainer key={game.id}>
+                        <GameCard game={game} />
                     </GameCardContainer>
                 ))}
             </SimpleGrid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,12 @@
-import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenre"
+import { HStack, List, ListItem, Image, Text, Spinner, Button } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenre"
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+    onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
     const { data, isLoading, error } = useGenres();
     if (error) return null;
     if (isLoading) return <Spinner />
@@ -11,7 +15,7 @@ const GenreList = () => {
             {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
                 <HStack>
                     <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                    <Text fontSize='lg'>{genre.name}</Text>
+                    <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
                 </HStack>
             </ListItem>)}
         </List>
