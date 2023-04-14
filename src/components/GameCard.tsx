44b7c444760964aaa074f675aea6a2c8bce71c23
rohit-kumar-globant <108795419+rohit-kumar-globant@users.noreply.github.com>
2023-04-14 13:25:47 +0530

    credit score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Image, Heading, Text } from "@chakra-ui/react"
+import { Card, CardBody, Image, Heading, Text, HStack } from "@chakra-ui/react"
 import { Game } from "../hooks/useGame"
+import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
 
 interface Props {
@@ -12,7 +13,11 @@ function GameCard({ game }: Props) {
             <Image src={game.background_image} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
-                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+                <HStack justifyContent={'space-between'}>
+                    <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
+
             </CardBody>
         </Card>
     )
