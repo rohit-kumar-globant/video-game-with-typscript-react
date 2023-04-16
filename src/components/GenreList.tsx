f40bce1794019f7eb4c5da619c12648bdf9760be
rohit-kumar-globant <108795419+rohit-kumar-globant@users.noreply.github.com>
2023-04-16 15:50:47 +0530

    customize theme

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -9,8 +9,10 @@ interface Props {
 
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     const { data, isLoading, error } = useGenres();
+
     if (error) return null;
     if (isLoading) return <Spinner />
+
     return (
         <>
             <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
